4663d9f820e6e8ed34ae8b3e9033b4d603c54fb1
d1b26 <105423175+d1b26@users.noreply.github.com>
2024-08-30 15:45:57 +0900

    fix: roulette animation

--- src/roulette.py
+++ src/roulette.py
@@ -7,13 +7,12 @@ import pygame
 class Roulette(pygame.sprite.Sprite):
     def __init__(self, rltname: str, xr: int, yr: int, backname: str, xb: int, yb: int):
         pygame.sprite.Sprite.__init__(self)
-        self.imagerlt = pygame.image.load(rltname).convert_alpha()
-        self.rectrlt = self.imagerlt.get_rect()
+        self.imagerltbase = pygame.image.load(rltname).convert_alpha()
+        self.rectrlt = self.imagerltbase.get_rect()
         self.rectrlt.topleft = (xr, yr)
         self.xr, self.yr = self.rectrlt.center
-        self.imagerlt = pygame.transform.rotate(self.imagerlt, 28)
-        self.rectrlt = self.imagerlt.get_rect()
-        self.rectrlt.center = (self.xr, self.yr)
+        self.rotate(28)
+        self.ang = 28
 
         self.imagebak = pygame.image.load(backname).convert_alpha()
         self.rectbak = self.imagerlt.get_rect()
@@ -22,15 +21,21 @@ class Roulette(pygame.sprite.Sprite):
     def draw(self, screen):
         screen.blit(self.imagebak, self.rectbak)
         screen.blit(self.imagerlt, self.rectrlt)
+        pygame.display.update()  # 画面を更新
 
     def run(self, screen) -> int:
         r = random.randrange(1, 5)
-
-        for _ in range(r):
-            time.sleep(0.5)
-            self.imagerlt = pygame.transform.rotate(self.imagerlt, 45)
-            self.rectrlt = self.imagerlt.get_rect()
-            self.rectrlt.center = (self.xr, self.yr)
+        now = self.ang // 45 + 1
+        times = r - now + 8
+        for _ in range(times):
+            time.sleep(0.3)
+            self.ang = (45 + self.ang) % 180
+            self.rotate(self.ang)
             self.draw(screen)
 
         return r
+
+    def rotate(self, ang: float):
+        self.imagerlt = pygame.transform.rotate(self.imagerltbase, ang)
+        self.rectrlt = self.imagerlt.get_rect()
+        self.rectrlt.center = (self.xr, self.yr)
